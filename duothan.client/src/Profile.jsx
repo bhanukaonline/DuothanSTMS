a4199a1dcdf098959887d9861ee531e58068d56a
Chandika365 <92178588+Chandika365@users.noreply.github.com>
2024-05-17 15:33:34 +0530

    Create Profile.jsx

---
+++ duothan.client/src/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+        {/* Profile */}
+        <div>
+            {/* this is heading */}
+         profile
+        </div>
+        <div>
+            
+            {/* user details */}
+            this section display details of above details
+            profile image
+            user id
+            user name
+            passwordHash
+            Email
+            full name
+            createdDate
+        </div>
+    </div>
+  )
+}
+
+export default Profile
